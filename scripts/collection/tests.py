308372f16a4c85ec2ce99928defe55ebcc25794e
Mikhail Kazantsev <ilkar@bk.ru>
2022-07-05 21:44:48 +0300

    App collection test

--- scripts/collection/tests.py
+++ scripts/collection/tests.py
@@ -18,6 +18,7 @@ def get_app_list(verbose = False):
     """
     Testing getting application list
     """
+    # Downloading application list
     try:
         full_ids = applist.get_app_list()
         full_ids.to_csv(f'{download_path}full_steam_ids.csv', index=False)
@@ -25,10 +26,31 @@ def get_app_list(verbose = False):
         if (verbose):
             print(e)
         return False
+    # Checking the downloaded application list
+    full_ids = pd.read_csv(f'{download_path}full_steam_ids.csv')
+    row_count = full_ids.shape[0]
+    ids_count = full_ids.drop_duplicates(subset='download_appid', keep='last').shape[0]
+    last_modified_na = full_ids.last_modified.isna().sum()
+    price_change_na = full_ids.price_change_number.isna().sum()
+    if (verbose):
+        print(f'Application IDs: {ids_count}')
+        print(f'Null last modified: {last_modified_na}')
+        print(f'Null price change: {price_change_na}')
+    if (ids_count < 10000):
+        return False
     return True
 
 def all(verbose = False):
-    get_app_list()
+    # App list download
+    if (get_app_list(verbose)):
+        print('App list download test PASSED')
+    else:
+        print('App list download test FAILED')
+    # Steam apps download
+
+    # SteamSpy apps download
+
+    # Steam Reviews download
     return True
 
-all()
+all(verbose = True)
